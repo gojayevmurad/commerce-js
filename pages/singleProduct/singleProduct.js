725658a06c587c62a0d73a87a682145ba1784ba3
Murad Qocayev <muradqocayevi20@gmail.com>
2023-03-09 12:46:03 +0400

    navbar design & single product page

---
+++ pages/singleProduct/singleProduct.js
@@ -0,0 +1,22 @@
+let category = $('.category')
+let productName = $('.product--name')
+let productName2  = $('.desc--name')
+let mainImage = $('.main-image')
+let productMainSlider = $('.product--main__slider')
+let price = $('.desc--price')
+
+
+
+
+// function createSingleProduct(){
+
+// }
+
+
+
+// window.onload = async function(){
+//     await onloadFunction()
+
+
+    
+// }
